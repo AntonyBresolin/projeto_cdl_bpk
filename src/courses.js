5feb6266bf136ed6a650b6e664952a5bcf707e58
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2023-10-24 13:19:28 -0300

    Dinamismo Cursos

--- src/courses.js
+++ src/courses.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    searchCourses();
+});
+
 async function searchCourses() {
     try {
         var result = await fetch('https://660q5f5r.api.sanity.io/v2021-10-21/data/query/production?query=%0A*%5B_type+%3D%3D+%22cursos%22%5D%7B%0A++nome_curso%0A%7D&perspective=published', {
@@ -12,9 +16,33 @@ async function searchCourses() {
     renderCourses(cursos);
 }
 function renderCourses(cursos) {
-    cursos.forEach(curso => {
+    const coursesAvaliable = document.querySelector('#cursos_select');
+    const optionsCourses = document.querySelector('#courses_avaliable');
+    const footerCourses = document.querySelector('#footer_courses');
+    cursos.forEach((curso, index) => {
         var nome = curso.nome_curso;
+        const option = document.createElement('option');
+        const course = document.createElement('option');
+        const footerLi = document.createElement('li');
+        const footerA = document.createElement('a');
+
+        //footerA.href = `./pages/${nome}.html`;
+        footerA.href = `#`;
+        footerA.textContent = nome;
+        footerLi.appendChild(footerA);
+        footerCourses.appendChild(footerLi);
+
+        course.value = nome;
+        course.textContent = nome;
+        option.value = nome;
+        option.textContent = nome;
+
+        if (optionsCourses) {
+            optionsCourses.appendChild(course);
+        }
+        if (coursesAvaliable) {
+            coursesAvaliable.appendChild(option);
+        }
         
     });
-
 }
