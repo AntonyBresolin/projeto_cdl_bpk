d51dba90bb0cc69d4c73abdbe605f94cdf14b424
Lucas Dreveck <lucasdrevc@outlook.com>
2023-10-25 14:44:23 -0300

    Faq dynamic

--- src/faq.js
+++ src/faq.js
@@ -1,8 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    searchFaq();
+});
+
 async function searchFaq() {
 
     try {
-        var result = await fetch("https://660q5f5r.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22pergunta_frequente%22%5D%7B%0A++pergunta%2C+resposta%0A%7D"
-            , {
+        var result = await fetch("https://660q5f5r.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22pergunta_frequente%22%5D%7B%0A++pergunta%2C+resposta%0A%7D", {
                 method: 'get'
             });
         var json = await result.json();
@@ -18,8 +21,37 @@ function renderFaqs(faqs) {
     faqs.forEach(faq => {
         var pergunta = faq.pergunta;
         var resposta = faq.resposta;
-        console.log(pergunta)
+        
+        var layout = document.getElementById("layout");
+        var pPergunta = document.createElement("p");
+        var pResposta = document.createElement("p");
+        var faq = document.createElement("div");
+        var faqPergunta = document.createElement("div");
+        var faqResposta = document.createElement("div");
+
+        faq.classList.add("accordion");
+        faqPergunta.classList.add("accordion__question");
+        faqResposta.classList.add("accordion__answer");
+        pPergunta.innerHTML = "+ " + pergunta;
+        pResposta.innerHTML = resposta;
+
+        faqPergunta.appendChild(pPergunta);
+        faqResposta.appendChild(pResposta);
+        faq.appendChild(faqPergunta);
+        faq.appendChild(faqResposta);
+        layout.appendChild(faq);
     });
 
+    accordionEvent();
+}
+
+function accordionEvent() {
+    const answers = document.querySelectorAll(".accordion");
+
+    answers.forEach((event) => {
+        event.addEventListener("click", () => {
+            event.classList.toggle("active");
+        });
+    });
 }
 
