e798aa3790eb4b810a83abcfbba9fe8fe4bbca76
Lucas Dreveck <lucas.dreveck@alunos.bpkedu.com.br>
2023-11-12 19:14:03 -0300

    Update API endpoints

--- src/reviews.js
+++ src/reviews.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 async function searchReviews() {
     try {
-        var result = await fetch("https://660q5f5r.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22avaliacao%22%5D%7B%0A++nome%2C+nota%2C+curso%2C+%22image%22%3A+imagem.asset-%3Eurl+%2Cdescricao%0A%7D", {
+        var result = await fetch("https://0o8j5e3g.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22avaliacao%22%5D%7B%0A++nome%2C+nota%2C+curso%2C+%22image%22%3A+imagem.asset-%3Eurl+%2Cdescricao%0A%7D", {
             method: 'get',
         });
         var json = await result.json();
