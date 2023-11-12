35bb0e394fa2ec9983af9158e52d25207e4173f5
Lucas Dreveck <lucas.dreveck@alunos.bpkedu.com.br>
2023-11-12 19:25:42 -0300

    Update api link

--- src/coordination.js
+++ src/coordination.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
 async function searchCoordination() {
 
     try {
-        var result = await fetch("https://660q5f5r.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22coordenacao%22%5D%7B%0A++nome%2C+genero+%2C%22image%22%3A+imagem.asset-%3Eurl%0A%7D", {
+        var result = await fetch("https://8lq8do82.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22coordenacao%22%5D%7B%0A++nome%2C+genero+%2C%22image%22%3A+imagem.asset-%3Eurl%0A%7D", {
             method: 'get',
         });
 
