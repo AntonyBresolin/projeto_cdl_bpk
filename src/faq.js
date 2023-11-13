48bd12fa0ed1da1f49b431c611730a78c9e3c191
Lucas Dreveck <lucas.dreveck@alunos.bpkedu.com.br>
2023-11-13 12:04:52 -0300

    Update api links to order by created

--- src/faq.js
+++ src/faq.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
 async function searchFaq() {
 
     try {
-        var result = await fetch("https://8lq8do82.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22pergunta_frequente%22%5D%7B%0A++pergunta%2C+resposta%0A%7D", {
+        var result = await fetch("https://8lq8do82.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22pergunta_frequente%22%5D%7C+order%28_createdAt+desc%29%7B%0A++pergunta%2C+resposta%0A%7D", {
                 method: 'get'
             });
         var json = await result.json();
--- src/reviews.js
+++ src/reviews.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 async function searchReviews() {
     try {
-        var result = await fetch("https://8lq8do82.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22avaliacao%22%5D%7B%0A++nome%2C+nota%2C+curso%2C+%22image%22%3A+imagem.asset-%3Eurl+%2Cdescricao%0A%7D", {
+        var result = await fetch("https://8lq8do82.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22avaliacao%22%5D%7C+order%28_createdAt+desc%29%7B%0A++nome%2C+nota%2C+curso%2C+%22image%22%3A+imagem.asset-%3Eurl+%2Cdescricao%0A%7D", {
             method: 'get',
         });
         var json = await result.json();
